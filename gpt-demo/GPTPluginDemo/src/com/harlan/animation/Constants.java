0eb5a13ce0ada4e89f5ff824f5e56c01cced782b
liuhaitao <liuhaitao@baidu.com>
2019-01-18 20:09:23 +0800

    【V6.3】【正式版】【修改】【Debug配置】

--- gpt-demo/GPTPluginDemo/src/com/harlan/animation/Constants.java
+++ gpt-demo/GPTPluginDemo/src/com/harlan/animation/Constants.java
@@ -24,7 +24,7 @@ package com.harlan.animation;
 public final class Constants {
 
     /**
-     * DEBUG 开关：默认为true，方便调试信息输出和问题定位。
+     * DEBUG 开关
      */
     public static final boolean DEBUG = true;
 
